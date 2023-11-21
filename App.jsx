00f7c63534180a45cc56bb8d3a4a1092013af99d
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-11-20 17:52:47 -0700

    App not running properly

--- App.jsx
+++ App.jsx
@@ -1,46 +1,32 @@
 import {
-  SafeAreaView,
-  StyleSheet
+  View,
+  Text
 } from "react-native";
 
-import {
-  NavigationContainer,
-} from "react-navigation/native";
+import { 
+  NavigationContainer 
+} from '@react-navigation/native';
 
-import {
-  createNativeStackNavigator
-} from "react-navigation/native-stack";
+import { 
+  createNativeStackNavigator 
+} from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create(
-  {
-    container: {
-      backgroundColor: "#f5f5dc"
-    },
-    homeScreen: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center"
-    }
-  }
-)
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
     </View>
-  )
+  );
 }
 
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-      </SafeAreaView>
+
     </NavigationContainer>
   )
 }
