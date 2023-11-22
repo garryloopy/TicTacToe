5dd55f47a75c20af6a7218dcc92dd3b97b6879b1
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-11-22 16:06:03 -0700

    Update SettingsPage.jsx

---
+++ screens/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import {
+    View,
+    Text,
+} from "react-native";
+
+export default function SettingsPage() {
+    return (
+        <View>
+            <Text>This is the settings page</Text>
+        </View>
+    )
+}
