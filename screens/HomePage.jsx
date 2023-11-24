df954c00780d4221ed09acdb5fd0e1a2efa99aaf
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-11-24 14:08:15 -0700

    Update HomePage.jsx

--- screens/HomePage.jsx
+++ screens/HomePage.jsx
@@ -1,5 +1,6 @@
 import {
     Button,
+    TouchableHighlight,
     View,
     Text,
     StyleSheet
@@ -7,28 +8,69 @@ import {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        marginTop: "auto",
+        marginBottom: "auto",
     },
     text: {
-        fontSize: 18,
+        fontSize: 30,
         marginBottom: 20,
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "#008000", 
+        textShadowColor: '#39FF14', // black shadow
+        textShadowOffset: { width: 3, height: 4 },
+        textShadowRadius: 1
     },
+    ButtonContainer: {
+        marginRight: 40,
+        marginLeft: 40,
+        marginBottom: 10,
+        backgroundColor: "#32CD32",
+        padding: 10,
+    },
+    ButtonContainer2: {
+        marginRight: 40,
+        marginLeft: 40,
+        marginBottom: 10,
+        backgroundColor: "green",
+        padding: 10,
+    },
+    
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 20,
+    }
 });
 
 export default function HomePage( {navigation} ) {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>
-                This is the home page
+                TIC TAC TOE
             </Text>
-            <Button title="Single play" 
-                    onPress={() => navigation.navigate("Game")}/>
-            <Button title="Duo play" 
-                    onPress={() => navigation.navigate("Game Duo")}/>
-            <Button title="Settings"
-                    onPress={() => navigation.navigate("Settings")}/>
+            
+            <TouchableHighlight
+                style={styles.ButtonContainer}
+                onPress={() => navigation.navigate("Game")}
+            >
+                <Text style={styles.buttonText}>Single play</Text>
+            </TouchableHighlight>
+            
+            <TouchableHighlight
+                style={styles.ButtonContainer2}
+                onPress={() => navigation.navigate("Game Duo")}
+            >
+                <Text style={styles.buttonText}>Duo play</Text>
+            </TouchableHighlight>
+            
+            <TouchableHighlight
+                style={styles.ButtonContainer}
+                onPress={() => navigation.navigate("Settings")}
+            >
+                <Text style={styles.buttonText}>Settings</Text>
+            </TouchableHighlight>
         </View>
     )
 }
